c45ad7e373d8443b77d2d1a745b790a384474849
[Clavelclams] <[clavelclams12@gmail.com]>
2024-03-27 16:54:41 +0100

    27/03 commande à faire

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-    console.log("ready ")
+    console.log("Prêt")
 
     $(`#bouton`).click(function()
     {
@@ -90,5 +90,98 @@ $(document).ready(function()
     // On peut bien sûr passer une variable
     var valeur = `Hello word`;
     $(`#champ`).val(valeur);
-    
+    function verif()
+    {
+        var envoi = true;
+        //  récupère la valeur saisie dans le champ input #prenom
+        var leprenom = $(`#prenom`).val();
+        if (leprenom == "")
+        {
+            envoie = false;
+            alert(`Le prénom doit être renseigné`);
+            console.log(`rien ne se passe`)
+        }
+        var lenom = $(`#nom`).val();
+        if (lenom == "")
+        {
+            envoi = false;
+            alert(`Le nom doit être renseigné`);
+        }
+        var lemail = $(`#mail`).val();
+        if (lemail == "")
+        {
+            envoi = false
+            alert(`Le mail doit être renseigné`)
+        }
+        var letel = $(`#tel`).val();
+        if (letel == "")
+        {
+            envoi = false
+            alert(`Le numero doit être renseigné`)
+        }
+        //    return envoi; //  Retourne la valeur de envoi
+        else
+        {
+            return false;
+        }
+    }
+    $("#btn_envoyer").click(function(e)
+    {
+        /* On doit bloquer l'èvènement par défaut avec l'instruction 
+        ci-dessous
+        `e` est un objet nommé librement représentant l'évènement */
+        e.preventDefault();
+        // Appel de la fonction verif()
+        verif();
+    })
+    console.log("Le plat suivant est prêt.");
+    var currentBurgerIndex = 1;
+    var totalBurgers = $('.burger-test').length;
+
+    // Cacher tous les burgers sauf le premier au chargement de la page
+    $('.burger-test').not('#burger1').hide();
+    console.log("test de ligne.");
+
+    // Gérer la navigation "Suivant"
+    $('#suivant').click(function() 
+    {
+        console.log("test de ligne.");
+
+        if (currentBurgerIndex < totalBurgers) 
+        {
+            $('#burger' + currentBurgerIndex).hide();
+            currentBurgerIndex++;
+            $('#burger' + currentBurgerIndex).show();
+            updateNavigationButtons();
+        }
+    });
+
+    // Gérer la navigation "Précédent"
+    $('#precedent').click(function() {
+        if (currentBurgerIndex > 1) {
+            $('#burger' + currentBurgerIndex).hide();
+            currentBurgerIndex--;
+            $('#burger' + currentBurgerIndex).show();
+            updateNavigationButtons();
+        }
+    });
+
+    // Met à jour l'état des boutons de navigation
+    function updateNavigationButtons() {
+        if (currentBurgerIndex === 1) {
+            $('#precedent').addClass('disabled');
+        } else {
+            $('#precedent').removeClass('disabled');
+        }
+
+        if (currentBurgerIndex === totalBurgers) {
+            $('#suivant').addClass('disabled');
+        } else {
+            $('#suivant').removeClass('disabled');
+        }
+    }
+    $(`.bouton-plat`).click(function(event)
+    {
+ 
+    }) 
 })
