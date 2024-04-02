500aeaa50ffda4d731a41f988d99202275c2a891
[Clavelclams] <[clavelclams12@gmail.com]>
2024-04-02 16:09:06 +0200

    excercie jquerry bientot l'evaluation faut redoubler d'effort

---
+++ exercice/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+    {
+        $("#btn1").click(function()
+        {
+            $("#div1").load("partiel1.html");
+        });
+        $("#btn2").click(function()
+        {
+            $("#div1").load("partiel2.html");
+        });
+    });
